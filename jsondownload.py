957b6e04fdfdf9f2b103af295ec9de5418ff1d4e
{kikutakyohei} <{kyohei.kikuta@hamadalab.com}>
2025-06-02 02:44:22 +0900

    recommend機能追加

--- jsondownload.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "https://kenkoooo.com/atcoder/resources/problems.json"
-response = requests.get(url)
-with open("problems.json", "w", encoding="utf-8") as f:
-    f.write(response.text)
